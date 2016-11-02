6004c6994e4f81803f077ae4670675ac8f6cbbce
oaktrk69 <oaktrk6969@gmail.com>
2016-11-02 23:48:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <title>AKILLI EV PROJESİ</title>
+        <title>AKILLI</title>
 		<meta charset="UTF-8"> 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
   <h2>1.LED</h2>
-	<button data-remodal-action="close" class="remodal-close"></button>
-	  
+	 <button class="button button-green" ng-click="resetPassword()">Submit</button>
+	 <button class="button button-green" ng-click="resetPassword()">Submit</button>  
 	
    <h2>2.led
    
-	  <button data-remodal-action="close" class="remodal-close"></button>
+	  <button class="button button-green" ng-click="resetPassword()">Submit</button>
 	  
 	
 		  <button class="button button-green" ng-click="resetPassword()">Submit</button>
