3f9b75828e1616f5e17999afd8224e6719a1e8f1
oaktrk69 <oaktrk6969@gmail.com>
2016-11-02 23:51:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
   <h2>1.LED</h2>
-	 <button class="button button-green" ng-click="resetPassword()">Submit</button>
-	 <button class="button button-green" ng-click="resetPassword()">Submit</button>  
+	 <button class="button button-green" ng-click="resetPassword()">On</button>
+	 <button class="button button-green" ng-click="resetPassword()">OFF</button>  
 	
-   <h2>2.led</h2>
+   <h2>2.LED</h2>
    
-	  <button class="button button-green" ng-click="resetPassword()">Submit</button>
+	  <button class="button button-green" ng-click="resetPassword()">ON</button>
 	  
 	
-		  <button class="button button-green" ng-click="resetPassword()">Submit</button>
+		  <button class="button button-green" ng-click="resetPassword()">OFF</button>
 	 
 
 </html>
